2a39b12be55d37fc71053d47899663bcd5dccb2b
Redouan El Rhazouani <81578195+redouan-rhazouani@users.noreply.github.com>
2022-11-30 17:39:29 +0100

    test consistency level ALL and QUORUM

--- usecases/replica/finder_test.go
+++ usecases/replica/finder_test.go
@@ -28,14 +28,14 @@ func object(id strfmt.UUID, lastTime int64) *storobj.Object {
 	}
 }
 
-func TestFinderFindWithConsistencyLevelALL(t *testing.T) {
+func TestFinderFind(t *testing.T) {
 	var (
 		id    = strfmt.UUID("123")
 		cls   = "C1"
 		shard = "SH1"
 		nodes = []string{"A", "B", "C"}
 		ctx   = context.Background()
-		obj   = object(id, 1)
+		obj   = object(id, 3)
 		adds  = additional.Properties{}
 		proj  = search.SelectProperties{}
 	)
@@ -49,5 +49,41 @@ func TestFinderFindWithConsistencyLevelALL(t *testing.T) {
 		got, err := finder.Find(ctx, "ALL", shard, id, proj, adds)
 		assert.Nil(t, err)
 		assert.Equal(t, obj, got)
+	})
+	t.Run("AllButLastOne", func(t *testing.T) {
+		f := newFakeFactory("C1", shard, nodes)
+		finder := f.newFinder()
+		for _, n := range nodes[:len(nodes)-1] {
+			f.Client.On("GetObject", anyVal, n, cls, shard, id, proj, adds).Return(obj, nil)
+		}
+		f.Client.On("GetObject", anyVal, nodes[len(nodes)-1], cls, shard, id, proj, adds).Return(object(id, 1), nil)
+		got, err := finder.Find(ctx, "ALL", shard, id, proj, adds)
+		assert.NotNil(t, err)
+		assert.Nil(t, got)
+		assert.Contains(t, err.Error(), "A: 3, B: 3, C: 1")
+	})
+	t.Run("AllButFirstOne", func(t *testing.T) {
+		f := newFakeFactory("C1", shard, nodes)
+		finder := f.newFinder()
+		for _, n := range nodes[1:] {
+			f.Client.On("GetObject", anyVal, n, cls, shard, id, proj, adds).Return(obj, nil)
+		}
+		f.Client.On("GetObject", anyVal, nodes[0], cls, shard, id, proj, adds).Return(object(id, 1), nil)
+		got, err := finder.Find(ctx, "ALL", shard, id, proj, adds)
+		assert.NotNil(t, err)
+		assert.Nil(t, got)
+		assert.Contains(t, err.Error(), "A: 1, B: 3, C: 3")
+	})
+	t.Run("NoQuorum", func(t *testing.T) {
+		f := newFakeFactory("C1", shard, nodes)
+		finder := f.newFinder()
+		for i, n := range nodes {
+			obj := object(id, int64(i+1))
+			f.Client.On("GetObject", anyVal, n, cls, shard, id, proj, adds).Return(obj, nil)
+		}
+		f.Client.On("GetObject", anyVal, nodes[0], cls, shard, id, proj, adds).Return(object(id, 1), nil)
+		got, err := finder.Find(ctx, "QUORUM", shard, id, proj, adds)
+		assert.Nil(t, got)
+		assert.Contains(t, err.Error(), "A: 1, B: 2, C: 3")
 	})
 }
